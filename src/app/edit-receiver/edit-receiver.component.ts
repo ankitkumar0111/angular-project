f2916af56fc9127469753f6e82eea0a73bd467fd
ankitkumar0111 <ankit324kumar@gmail.com>
2023-11-08 10:16:14 +0530

    Edit receiver

--- src/app/edit-receiver/edit-receiver.component.ts
+++ src/app/edit-receiver/edit-receiver.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReceiverService } from '../receiver.service';
 
 @Component({
   selector: 'app-edit-receiver',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditReceiverComponent implements OnInit {
 
-  constructor() { }
+  receiverForm: FormGroup;
+  receiverId: number;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder, 
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private receiverService: ReceiverService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.receiverId = +params['id'];
+      this.receiverService.getReceiver(this.receiverId).subscribe(receiver => {
+        this.populateForm(receiver);
+      });
+    });
+  }
+
+  populateForm(receiver: any) {
+    this.receiverForm.patchValue({
+      firstName: receiver.firstName,
+      lastName: receiver.lastName,
+      // Add other form fields here
+    });
+  }
+
+  onSubmit() {
+    const receiverData = this.receiverForm.value;
+    this.receiverService.updateReceiver(this.receiverId, receiverData).subscribe(response => {
+      // Handle response if needed
+      this.router.navigate(['/my-receiver']);
+    });
   }
 
 }
