3ef0583f04872fa953663a204882f1e1da1bc9ac
ankitkumar0111 <ankit324kumar@gmail.com>
2023-11-21 11:03:01 +0530

    New update

--- src/app/add-receiver/add-receiver.component.ts
+++ src/app/add-receiver/add-receiver.component.ts
@@ -1,95 +1,97 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ReceiverService } from 'src/app/receiver.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ReceiverService } from "src/app/receiver.service";
 
 @Component({
-  selector: 'app-add-receiver',
-  templateUrl: './add-receiver.component.html',
-  styleUrls: ['./add-receiver.component.css']
+  selector: "app-add-receiver",
+  templateUrl: "./add-receiver.component.html",
+  styleUrls: ["./add-receiver.component.css"],
 })
 export class AddReceiverComponent implements OnInit {
-
   receiverForm: FormGroup;
   countries: any[];
 
-  constructor(private fb: FormBuilder, private receiverService: ReceiverService,private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private receiverService: ReceiverService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.receiverForm = this.fb.group({
-      country: ['', Validators.required],
-      flagImageUrl: [''],
-      type: ['',Validators.required], 
-      countryCode: ['',Validators.required], 
-      phoneNumber: ['',Validators.required], 
-      firstName: ['',Validators.required],
-      middleName: ['',Validators.required],
-      lastName: ['',Validators.required]
+      country: ["", Validators.required],
+      flagImageUrl: [""],
+      type: ["", Validators.required],
+      countryCode: ["", Validators.required],
+      phoneNumber: ["", Validators.required],
+      firstName: ["", Validators.required],
+      middleName: ["", Validators.required],
+      lastName: ["", Validators.required],
     });
 
-   
-
-    this.receiverService.getCountries().subscribe(countries => {
+    this.receiverService.getCountries().subscribe((countries) => {
       this.countries = countries;
       console.log(countries);
-      
     });
 
-    this.receiverForm.get('type').disable();
-    this.receiverForm.get('countryCode').disable();
-    this.receiverForm.get('phoneNumber').disable();
-    this.receiverForm.get('firstName').disable();
-    this.receiverForm.get('middleName').disable();
-    this.receiverForm.get('lastName').disable();
+    this.receiverForm.get("type").disable();
+    this.receiverForm.get("countryCode").disable();
+    this.receiverForm.get("phoneNumber").disable();
+    this.receiverForm.get("firstName").disable();
+    this.receiverForm.get("middleName").disable();
+    this.receiverForm.get("lastName").disable();
   }
 
   get firstName() {
-    return this.receiverForm.get('firstName');
+    return this.receiverForm.get("firstName");
   }
 
   get middleName() {
-    return this.receiverForm.get('middleName');
+    return this.receiverForm.get("middleName");
   }
-
 
   get lastName() {
-    return this.receiverForm.get('lastName');
+    return this.receiverForm.get("lastName");
   }
 
- 
   onSubmit() {
     if (this.receiverForm.invalid) {
       return;
     }
     const receiverData = this.receiverForm.value;
-    this.receiverService.postReceiver(receiverData).subscribe(response => {
-      this.router.navigate(["/myreceiver"])  
+    this.receiverService.postReceiver(receiverData).subscribe((response) => {
+      this.router.navigate(["/myreceiver"]);
     });
-    
   }
 
-
   onCountryChange() {
-    const selectedCountry = this.receiverForm.get('country').value;
-    const countryDetails = this.countries.find(country => country.name === selectedCountry);
+    const selectedCountry = this.receiverForm.get("country").value;
+    const countryDetails = this.countries.find(
+      (country) => country.name === selectedCountry
+    );
 
-   
     const controlsToEnable = countryDetails.fields;
-    controlsToEnable.forEach(control => {
+    controlsToEnable.forEach((control) => {
       this.receiverForm.get(control).enable();
     });
 
-
-    const controlsToDisable = ['type', 'countryCode', 'phoneNumber', 'firstName', 'middleName', 'lastName'];
-    controlsToDisable.forEach(control => {
+    const controlsToDisable = [
+      "type",
+      "countryCode",
+      "phoneNumber",
+      "firstName",
+      "middleName",
+      "lastName",
+    ];
+    controlsToDisable.forEach((control) => {
       if (!controlsToEnable.includes(control)) {
         this.receiverForm.get(control).disable();
       }
     });
   }
 
-  navigateToMyReceiver(){
-    this.router.navigate(['/myreceiver']);
+  navigateToMyReceiver() {
+    this.router.navigate(["/myreceiver"]);
   }
-
 }
